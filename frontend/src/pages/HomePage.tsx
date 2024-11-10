aa6941f813472518498e4f6245fd668840f0abed
Dafina Palucaj <dafi.palucaj@gmail.com>
2024-11-10 18:50:38 +0100

    Initial commit

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Event Management System</h1>
+      <p>Here you can create, view, and register for many different events!   </p>  
+      <p> </p>
+      <p>Whether you want to attend, organize, or explore, we have something for everyone.</p>
+      <ul>
+      </ul>
+    </div>
+  );
+};
+
+export default HomePage;
