c2fa7ba126254fff7fe7e7e0e118bc75e9f1c848
Dafina Palucaj <dafi.palucaj@gmail.com>
2024-11-17 01:48:53 +0100

    Fix CORS configuration in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,19 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
-import eventRoutes from './routes/eventRoutes.js';  
-const fastify = Fastify({ logger: true });
+import eventRoutes from './routes/eventRoutes.js';
 
+const fastify = Fastify({ logger: true });
 const prisma = new PrismaClient();
 
-fastify.register(cors);
+// Register CORS with specific options
+fastify.register(cors, {
+  origin: ['https://event-management-system-rouge-rho.vercel.app/'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+});
 
-fastify.register(eventRoutes); 
+fastify.register(eventRoutes);
 
 fastify.get('/', async (request, reply) => {
   return { message: 'Welcome to the Event Management System API!' };
@@ -17,7 +22,7 @@ fastify.get('/', async (request, reply) => {
 const startServer = async () => {
   try {
     await prisma.$connect();
-    await fastify.listen({ port: 3000, host: 'localhost' }); 
+    await fastify.listen({ port: 3000, host: '0.0.0.0' }); 
     console.log(`Server is running on http://localhost:3000`);
   } catch (error) {
     fastify.log.error(error);
@@ -25,7 +30,7 @@ const startServer = async () => {
   }
 };
 
-
+// Start the server
 startServer();
 
 process.on('SIGINT', async () => {
